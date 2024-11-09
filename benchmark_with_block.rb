ef52185d8b12d6904634e0e60979ffa56cc24772
Saif Al-saedi <saif4004@gmail.com>
2024-11-09 13:07:20 -0800

    implemented benchmark

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,9 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now
+  result_from_block = yield
+  end_time = Time.now
+  running_time = end_time - start_time
+  running_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
